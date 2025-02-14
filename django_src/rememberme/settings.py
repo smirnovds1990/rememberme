01e89000face5dba0a106e0d6a49c029a80e2a4a
smirnovds1990 <smirnovds1990@gmail.com>
2025-02-14 13:10:31 +0300

    feat: switch to use PostgreSQL

--- django_src/rememberme/settings.py
+++ django_src/rememberme/settings.py
@@ -56,8 +56,12 @@ WSGI_APPLICATION = "rememberme.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 

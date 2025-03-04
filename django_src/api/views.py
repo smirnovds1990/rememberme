b56044bb9584dafd24a7f1d5f97864874be852ea
smirnovds1990 <smirnovds1990@gmail.com>
2025-03-04 12:21:27 +0300

    feat: add API to manage User instances with a schedule

---
+++ django_src/api/views.py
@@ -0,0 +1,42 @@
+from api.serializers import UserSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import (
+    CreateModelMixin,
+    DestroyModelMixin,
+    UpdateModelMixin,
+)
+
+from django_src.users.models import User
+
+
+class ScheduleCreateView(
+    CreateModelMixin,
+    GenericAPIView,
+):
+    """Create a user messaging schedule.
+
+    Get a username, wanted topics, schedule hours from the bot
+    and create a user instance.
+    """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class ScheduleUpdateDestroyView(
+    DestroyModelMixin, UpdateModelMixin, GenericAPIView
+):
+    """Update or delete a user messaging schedule."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "username"
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)

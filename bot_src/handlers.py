5dfdcc9d2a27f881a1249afc654ecba1bd09efad
smirnovds1990 <smirnovds1990@gmail.com>
2025-03-28 11:13:35 +0300

    feat: add cancel_action handler

--- bot_src/handlers.py
+++ bot_src/handlers.py
@@ -1,9 +1,10 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, ReplyKeyboardRemove
 
 from constants import START_MESSAGE
-from keyboards import main_keyboard
+from keyboards import cancel_keyboard, main_keyboard
 
 
 main_router = Router()
@@ -17,4 +18,10 @@ async def start(message: Message) -> None:
 
 @main_router.message(F.text == "Start registration.")
 async def start_registration(message: Message):
-    await message.answer("Let's start.")
+    await message.answer("Let's start.", reply_markup=cancel_keyboard)
+
+
+@main_router.message(F.text == "Cancel")
+async def cancel_action(message: Message, state: FSMContext) -> None:
+    await state.clear()
+    await message.answer("Cancelled", reply_markup=ReplyKeyboardRemove())
--- bot_src/keyboards.py
+++ bot_src/keyboards.py
@@ -1,6 +1,14 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 
+cancel_button = KeyboardButton(text="Cancel")
+cancel_keyboard = ReplyKeyboardMarkup(
+    keyboard=[[cancel_button]],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+)
+
+
 def main_keyboard() -> ReplyKeyboardMarkup:
     buttons = [[KeyboardButton(text="Start registration.")]]
     return ReplyKeyboardMarkup(

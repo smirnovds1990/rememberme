33af513c5cf17312661087e16695da3bd33bc0cc
smirnovds1990 <smirnovds1990@gmail.com>
2025-05-06 13:11:44 +0300

    feat: add RandomArticleRetrieveView

--- django_src/api/serializers.py
+++ django_src/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
+from articles.models import Article
 from users.models import User
 
 
@@ -13,3 +14,9 @@ class UserSerializer(ModelSerializer):
             "topics",
             "messages_schedule",
         )
+
+
+class ArticleSerializer(ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ("title", "content")
--- django_src/api/views.py
+++ django_src/api/views.py
@@ -1,11 +1,17 @@
+import random
+
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import (
     CreateModelMixin,
     DestroyModelMixin,
     UpdateModelMixin,
 )
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from api.serializers import UserSerializer
+from api.serializers import ArticleSerializer, UserSerializer
+from articles.exceptions import NoArticlesException
+from articles.models import Article
 from users.models import User
 
 
@@ -23,7 +29,7 @@ class ScheduleCreateView(
     serializer_class = UserSerializer
 
     def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        return super().create(request, *args, **kwargs)
 
 
 class ScheduleUpdateDestroyView(
@@ -36,7 +42,20 @@ class ScheduleUpdateDestroyView(
     lookup_field = "username"
 
     def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
+        return super().partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+        return super().destroy(request, *args, **kwargs)
+
+
+class RandomArticleRetrieveView(APIView):
+    """Get a random article to send it via bot."""
+
+    def get(self, request):
+        articles_amount = Article.objects.count()
+        if articles_amount == 0:
+            raise NoArticlesException("Sorry. There are no articles yet.")
+        random_index = random.randint(0, articles_amount - 1)
+        random_article = Article.objects.all()[random_index]
+        serializer = ArticleSerializer(random_article)
+        return Response(serializer.data)

6899182dc913d7b1848616f4e55ac6a4a2a46a9f
smirnovds1990 <smirnovds1990@gmail.com>
2025-06-25 11:35:38 +0300

    feat: add "makemigartions" command to django_entrypoint.sh

--- django_src/django_entrypoint.sh
+++ django_src/django_entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 uv run manage.py collectstatic --noinput
+uv run manage.py makemigrations --noinput
 uv run manage.py migrate --noinput
 uv run gunicorn rememberme.wsgi:application --bind ${HOST}:${PORT}

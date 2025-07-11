03b5a2b0726c88003735f3422c10e773f457a7aa
smirnovds1990 <smirnovds1990@gmail.com>
2025-07-11 10:42:50 +0300

    refactor: add form_message()

--- bot_src/utils.py
+++ bot_src/utils.py
@@ -4,16 +4,20 @@ from constants import RANDOM_ARTICLE_URL
 from exceptions import RequestException
 
 
+def form_message(data: dict[str, str]) -> str:
+    title = data["title"]
+    content = data["content"]
+    content = content.replace("\r\n", "\n")
+    return f"<b>{title}</b>\n\n{content}"
+
+
 async def get_random_article():
     async with httpx.AsyncClient() as client:
         try:
             response = await client.get(RANDOM_ARTICLE_URL)
             response.raise_for_status()
             data = response.json()
-            title = data["title"]
-            content = data["content"]
-            content = content.replace("\r\n", "\n")
-            return f"<b>{title}</b>\n\n{content}"
+            return form_message(data)
         except httpx.RequestError as exc:
             raise RequestException(
                 f"An error occurred while requesting {exc.request.url!r}."

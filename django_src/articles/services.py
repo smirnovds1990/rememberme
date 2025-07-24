20026d18af208e652c809674be6af69bfd4c25a3
smirnovds1990 <smirnovds1990@gmail.com>
2025-07-24 16:37:04 +0300

    Add logging to ArticleService

--- django_src/articles/services.py
+++ django_src/articles/services.py
@@ -1,14 +1,27 @@
+import logging
+
+from articles.exceptions import NoArticlesException
 from articles.models import Article
 from articles.repository import ArticleRepository
+
+
+logger = logging.getLogger(__name__)
 
 
 class ArticleService:
     """Provide business logic methods for Article."""
 
     def get_random_article_and_refresh_stats(self) -> Article:
-        articles_amount = ArticleRepository().count_articles()
-        random_article = ArticleRepository().get_random_article(
-            articles_amount=articles_amount
-        )
-        ArticleRepository().save_stats(article=random_article)
-        return random_article
+        try:
+            articles_amount = ArticleRepository().count_articles()
+            random_article = ArticleRepository().get_random_article(
+                articles_amount=articles_amount
+            )
+            ArticleRepository().save_stats(article=random_article)
+            logger.info(
+                "Send and refresh stats of artcile: %s", random_article.title
+            )
+            return random_article
+        except NoArticlesException as e:
+            logger.warning("No articles available: %s", e)
+            raise
